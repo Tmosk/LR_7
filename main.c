85f006336617a48d761f535f3fa3266148762ed0
Tsimophey <t.moskalev.w@gmai.com>
2022-06-11 07:38:46 +0300

    Done

--- main.c
+++ main.c
@@ -17,11 +17,10 @@ list generate(int *k);
 
 void print(list li);
 
-list sort(list li);
+int sort(list li);
 
 node *get(list li, int i);
 
-list swap(node *a, node *b, list li);
 
 int main() {
     int k;
@@ -29,8 +28,7 @@ int main() {
     scanf_s("%d", &k);
     printf("enter data:\n");
     list li = generate(&k);
-    li = sort(li);
-    print(li);
+    sort(li)? printf("Sorted"): printf("Not sorted");
     return 0;
 }
 
@@ -63,94 +61,17 @@ void print(list li) {
     printf("%s\n", tmp->data);
 }
 
-list sort(list li) {
+int sort(list li) {
     int count = li.size;
     for (int i = 0; i < count; i++)
         for (int j = i + 1; j < count; j++) {
             if (strcmp(get(li, i)->data, get(li, j)->data) < 0) {
-                li = swap(get(li, i), get(li, j), li);
+                return 0;
             }
         }
-    return li;
+    return 1;
 }
 
-list swap(node *a, node *b, list li) {
-    if ((a == NULL) || (b == NULL)) {
-        return li;
-    }
-    node *tmp;
-    if (li.head == a)li.head = b;
-    else if (li.head == b)li.head = a;
-    if (li.tail == a)li.tail = b;
-    else if (li.tail == b)li.tail = a;
-
-    if (a->next == b) {
-        a->next = b->next;
-        if (b->next != NULL) {
-            b->next->prev = a;
-        }
-        b->next = a;
-        b->prev = a->prev;
-        if (a->prev != NULL) {
-            a->prev->next = b;
-        }
-        a->prev = b;
-    } else if (b->next == a) {
-        b->next = a->next;
-        if (a->next != NULL) {
-            a->next->prev = b;
-        }
-        a->next = b;
-        a->prev = b->prev;
-        if (b->prev != NULL) {
-            b->prev->next = a;
-        }
-        b->prev = a;
-    } else if (a->prev == b) {
-        a->prev = b->prev;
-        if (b->prev != NULL) {
-            b->prev->next = a;
-        }
-        b->prev = a;
-        b->next = a->prev;
-        if (a->next != NULL) {
-            a->next->prev = b;
-        }
-        a->next = b;
-    } else if (b->prev == a) {
-        b->prev = a->prev;
-        if (a->prev != NULL) {
-            a->prev->next = b;
-        }
-        a->prev = b;
-        a->next = b->prev;
-        if (b->next != NULL) {
-            b->next->prev = a;
-        }
-        b->next = a;
-    } else {
-        if (a->prev != NULL) {
-            a->prev->next = b;
-        }
-        if (a->next != NULL) {
-            a->next->prev = b;
-        }
-        if (b->prev != NULL) {
-            b->prev->next = a;
-        }
-        if (b->next != NULL) {
-            b->next->prev = a;
-        }
-        tmp = a->next;
-        a->next = b->next;
-        b->next = tmp;
-        tmp = a->prev;
-        a->prev = b->prev;
-        b->prev = tmp;
-    }
-
-    return li;
-}
 
 node *get(list li, int i) {
     node *tmp = li.head;
